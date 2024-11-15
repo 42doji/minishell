4674bd2e2228d4df5335cbeaf067557e2626602a
Junheon Min <junmin@c2r8s1.42gyeongsan.kr>
2024-11-15 21:12:37 +0900

    fix only ifs

--- srcs/parser/init.c
+++ srcs/parser/init.c
@@ -6,11 +6,61 @@
 /*   By: junmin <junmin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:43:27 by junmin            #+#    #+#             */
-/*   Updated: 2024/11/14 20:20:21 by junmin           ###   ########.fr       */
+/*   Updated: 2024/11/15 21:11:25 by junmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	count_token(char *str, int i)
+{
+	while (str[i] && !is_ifs(str[i]))
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			i = skip_quote(str, i);
+			if (str[i] == '|' || str[i] == '<' || str[i] == '>')
+				return (i);
+		}
+		else if (str[i] == '|')
+			return (++i);
+		else if (str[i] == '>' || str[i] == '<')
+		{
+			if (str[i + 1] == '>' || str[i + 1] == '<')
+				i++;
+			return (++i);
+		}
+		else if (str[i + 1] == '|')
+			return (++i);
+		else if (str[i + 1] == '<' || str[i + 1] == '>')
+			return (++i);
+		else
+			i++;
+	}
+	return (i);
+}
+
+static int	calculate_token_count(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && is_ifs(str[i]))
+			i++;
+		if (str[i] && !is_ifs(str[i]))
+		{
+			i = count_token(str, i);
+			count++;
+		}
+		if (str[i] == '\0')
+			break ;
+	}
+	return (count);
+}
 
 void	initialize_shell(t_minishell *mini)
 {
@@ -21,6 +71,9 @@ void	initialize_shell(t_minishell *mini)
 	mini->in = dup(STDIN_FILENO);
 	mini->out = dup(STDOUT_FILENO);
 	add_history(mini->str);
+	mini->n_tokens = calculate_token_count(mini->str);
+	if (mini->n_tokens == 0)
+		return ;
 	lexer(mini);
 	if (check_unfinished_quote(mini->input) == 1)
 		return ;
--- srcs/parser/lexer.c
+++ srcs/parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: junmin <junmin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:43:34 by junmin            #+#    #+#             */
-/*   Updated: 2024/11/10 16:27:00 by junmin           ###   ########.fr       */
+/*   Updated: 2024/11/15 21:11:20 by junmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,56 +40,6 @@ static int	save_token_special(char *str, int i)
 	return (i - 1);
 }
 
-static int	count_token(char *str, int i)
-{
-	while (str[i] && !is_ifs(str[i]))
-	{
-		if (str[i] == '\'' || str[i] == '"')
-		{
-			i = skip_quote(str, i);
-			if (str[i] == '|' || str[i] == '<' || str[i] == '>')
-				return (i);
-		}
-		else if (str[i] == '|')
-			return (++i);
-		else if (str[i] == '>' || str[i] == '<')
-		{
-			if (str[i + 1] == '>' || str[i + 1] == '<')
-				i++;
-			return (++i);
-		}
-		else if (str[i + 1] == '|')
-			return (++i);
-		else if (str[i + 1] == '<' || str[i + 1] == '>')
-			return (++i);
-		else
-			i++;
-	}
-	return (i);
-}
-
-static int	calculate_token_count(char *str)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		while (str[i] && is_ifs(str[i]))
-			i++;
-		if (str[i] && !is_ifs(str[i]))
-		{
-			i = count_token(str, i);
-			count++;
-		}
-		if (str[i] == '\0')
-			break ;
-	}
-	return (count);
-}
-
 static void	save_token(t_minishell *mi)
 {
 	int	i;
@@ -120,7 +70,6 @@ static void	save_token(t_minishell *mi)
 
 void	lexer(t_minishell *mini)
 {
-	mini->n_tokens = calculate_token_count(mini->str);
 	mini->input = malloc(sizeof(char *) * (mini->n_tokens + 1));
 	if (!mini->input)
 		exit(1);
